1bb333afd351630424841c7edafb42308a3c80ff
samnanglath01 <112363663+samnanglath01@users.noreply.github.com>
2024-03-08 20:49:36 -0800

    tonight

---
+++ app/src/main/java/com/example/beachtest/MyFirebaseMessagingService.kt
@@ -0,0 +1,53 @@
+package com.example.beachtest.Notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.beachtest.MainActivity
+import com.example.beachtest.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        // Create a simple notification
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationID = Random.nextInt()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntentFlags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        } else {
+            PendingIntent.FLAG_ONE_SHOT
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, pendingIntentFlags)
+
+        val notificationBuilder = NotificationCompat.Builder(this, "YOUR_CHANNEL_ID")
+            .setContentTitle(remoteMessage.notification?.title ?: "Default Title")
+            .setContentText(remoteMessage.notification?.body ?: "Default Text")
+            .setSmallIcon(R.drawable.ic_notification) // make sure you have this icon in your drawables
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("YOUR_CHANNEL_ID", "Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(notificationID, notificationBuilder.build())
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/beachtest/Users/ui/UserManager.kt
@@ -0,0 +1,4 @@
+package com.example.beachtest.Users.ui
+
+class UserManager {
+}
